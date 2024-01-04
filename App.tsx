34fa2d774e15e71c0d8a2f0216fd7b8e1c27aa93
BuildTools <unconfigured@null.spigotmc.org>
2024-01-03 22:21:18 -0300

    list of task add and remove task list

--- App.tsx
+++ App.tsx
@@ -1,11 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  FlatList,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import Tarefa from "./src/Tarefa";
+
+type TarefaType = {
+  key: number;
+  item: string;
+};
 
 export default function App() {
+  const [tarefa, setTarefa] = useState("");
+  const [list, setList] = useState<TarefaType[]>([]);
+
+  function handleAdd() {
+    if (tarefa === "") {
+      return;
+    }
+
+    const dados = {
+      key: Date.now(),
+      item: tarefa,
+    };
+
+    setList((oldArray) => [dados, ...oldArray]);
+
+    setTarefa("");
+  }
+
+  function handleDelete(item: any) {
+    let filtroItem = list.filter((tarefa) => {
+      return tarefa.item !== item;
+    });
+
+    setList(filtroItem);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Tarefas</Text>
+
+      <View style={styles.containerInput}>
+        <TextInput
+          placeholder="Digite sua tarefa..."
+          style={styles.input}
+          value={tarefa}
+          onChangeText={(text) => setTarefa(text)}
+        />
+
+        <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
+          <FontAwesome name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={list}
+        keyExtractor={(item) => item.key.toString()}
+        renderItem={({ item }) => (
+          <Tarefa data={item} deleteItem={() => handleDelete(item.item)} />
+        )}
+        style={styles.list}
+      />
     </View>
   );
 }
@@ -13,8 +74,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#22272e",
+    paddingTop: 28,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    color: "#FFF",
+    marginTop: "5%",
+    paddingStart: "5%",
+    marginBottom: 12,
+  },
+  containerInput: {
+    flexDirection: "row",
+    width: "100%",
+    height: 44,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 22,
+  },
+  input: {
+    width: "75%",
+    backgroundColor: "#FBFBFB",
+    height: 44,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+  },
+  buttonAdd: {
+    width: "15%",
+    height: 44,
+    backgroundColor: "#73f7ff",
+    marginLeft: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    paddingStart: "4%",
+    paddingEnd: "4%",
   },
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+## App for adding and removing items from a list
+
+Technologies used:
+
+- React Native
+- Expo
+- Styled Sheets
